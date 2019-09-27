2e4afe28067db412bb6272ed0fc31890ee0d3711
Unlocked <10186337+TheUnlocked@users.noreply.github.com>
2019-09-27 11:48:20 -0400

    Changed release file extension to .min.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,6 @@ module.exports = {
   mode: "production",
   entry: "./dist/box-and-pointer.js",
   output: {
-    filename: "../release/box-and-pointer.js"
+    filename: "../release/box-and-pointer.min.js"
   }
 }
